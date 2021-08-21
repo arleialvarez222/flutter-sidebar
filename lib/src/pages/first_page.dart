d9c9e5172b75c54d6b33939dacb4e22053773ba7
arleialvarez222 <alvarezarlei222@gmail.com>
2021-08-21 14:36:50 -0500

    first commit

---
+++ lib/src/pages/first_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('First page'),
+      ),
+    );
+  }
+}
